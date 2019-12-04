38b9f8e257b8319e7fea1a8c7da35d6aa08fdc74
wujiankun <wujiankun@corp.netease.com>
2019-12-04 18:42:26 +0800

    opt day1

--- day1/removeDuplicates.js
+++ day1/removeDuplicates.js
@@ -2,19 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-function removeDuplicates(nums) {
+var removeDuplicates = function(nums) {
     if(!Array.isArray(nums)||!nums) return -1;
     if (nums.length <= 1) return nums.length;
-    let i = 0;
-    for (let j = 1; j < nums.length; j++) {
-        if (nums[j] !== nums[i]) {
+    let i=0;
+    for(let j=1;j<nums.length;j++){
+        if(nums[i]!==nums[j]){
             i++;
-            nums[i] = nums[j];
+            if(nums[i]!==nums[j]){
+                console.log("do copy from "+j+"to "+i);
+                nums[i]=nums[j];
+            }
         }
     }
-    return i + 1;
-}
-
-let resultLen = removeDuplicates([1,2,3,4,4,4,4,8]);
-
-console.log(resultLen);
+    return i+1;
+};
+let target = [1,2,2,2,2,2,3,4,4,4,4,8];
+removeDuplicates(target);
+console.log(target);
